bc505d7ae1ae716c3e47dab70688c20aea0fe9e4
Zack Piper <zack.piper123@gmail.com>
2014-10-12 11:03:03 +0100

    Stuff.

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,4 +1,11 @@
+require 'json'
 class ListController < ApplicationController
   def index
+    list = []
+    Dir.glob('../ipindx/index/*.json').each do |n|
+      json_file = File.read n
+
+    end
+    @list = list
   end
 end
