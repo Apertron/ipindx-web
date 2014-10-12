8a26bf7f9579ba278331b807262f5fdf8990f224
Zack Piper <zack.piper123@gmail.com>
2014-10-12 12:16:32 +0100

    Add basic list.

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -3,8 +3,11 @@ class ListController < ApplicationController
   def index
     list = []
     Dir.glob('../ipindx/index/*.json').each do |n|
-      json_file = File.read n
+      json_processed = JSON.load File.read n
 
+      # TODO: search bar
+
+      list << json_processed
     end
     @list = list
   end
